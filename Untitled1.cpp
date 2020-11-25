a5ae1f8b23aaed54abc261d51ba273732ceec1f4
00757107 <58518511+00757107@users.noreply.github.com>
2020-11-25 23:21:22 +0800

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,16 @@
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);
+
+	printf("hey!");
+
+	printf("hey!1231231246556");
+
+	
+	
+	return 0;
+}
+
